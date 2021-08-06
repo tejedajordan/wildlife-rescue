c1e85eb22c52c7aaa76aacbf90fc37d20f8e2eb8
Jordan Tejeda <tejedajordan@gmail.com>
2021-08-06 16:00:43 -0400

    this is the project for wildlife rescue website

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Jordan Tejeda
+    File Name: script.js
+    Date: 06/05/2021
+*/
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
